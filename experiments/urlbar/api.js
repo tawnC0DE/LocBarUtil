17275c024cccc518bdd1e97567a8b43e5f3b5332
Tawn <149983234+tawnC0DE@users.noreply.github.com>
2026-01-30 20:53:40 -0500

    Support Firefox 147+

--- experiments/urlbar/api.js
+++ experiments/urlbar/api.js
@@ -26,14 +26,14 @@ this.experiments_urlbar = class extends ExtensionAPI {
             // if user clicks icon before clicking off of urlbar, urlbar blur el may not have triggered yet
             if (win.document.activeElement.id == "urlbar-input") {
               console.log("blurring urlbar");
-              win.gURLBar.view.input.blur();  // only do this if urlbar still has focus!
+              win.gURLBar.inputField.blur();  // only do this if urlbar still has focus!
             }
             return searchResults;
           },
           async initURLbarListener() {
             console.log("initURLbarListener called");
             let win = BrowserWindowTracker.getTopWindow();
-            let URLbarInput = win.gURLBar.view.input;
+            let URLbarInput = win.gURLBar.inputField;
             blurListener = this.captureSearchResults;
             URLbarInput.addEventListener('blur', blurListener);
             console.log("listening");
@@ -42,7 +42,7 @@ this.experiments_urlbar = class extends ExtensionAPI {
           async uninitURLbarListener() {
             console.log("uninitURLbarListener called");
             let win = BrowserWindowTracker.getTopWindow();
-            let URLbarInput = win.gURLBar.view.input;
+            let URLbarInput = win.gURLBar.inputField;
             URLbarInput.removeEventListener('blur', this.captureSearchResults);
             console.log("listener removed");
           },
@@ -77,7 +77,7 @@ this.experiments_urlbar = class extends ExtensionAPI {
       return; // just let Fx unload it!
     }
     let win = BrowserWindowTracker.getTopWindow();
-    let URLbarInput = win.gURLBar.view.input;
+    let URLbarInput = win.gURLBar.inputField;
     URLbarInput.removeEventListener('blur', blurListener);
     console.log("blurListener removed");
   }
